7a10268864d51771a55792e1ae9dabba3ac76980
Viktoria Myshlionok <vmyshlionok@gmail.com>
2026-01-01 20:35:21 +0300

    Homework_5 done

---
+++ vika_homework/homework_5/test.py
@@ -0,0 +1,15 @@
+my_list = [2,6]
+a, b = my_list # нужно знать количество элементов списка
+print(a, b)
+
+text = 'my long long string'
+print(text[3])
+print(len(text))
+print(text.index('long'))
+print('long' in text)
+print(text.count('n'))
+print(text.count('long'))
+print(text.find('lone'))
+print(text[:7])
+print(text.startswith('my'))
+print(text.endswith('ng'))
