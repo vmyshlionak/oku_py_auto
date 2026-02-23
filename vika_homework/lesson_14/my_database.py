cb910c620a836fc513ff964b23e3e1e29a6e41a1
Viktoria Myshlionok <vmyshlionok@gmail.com>
2026-02-23 23:08:29 +0300

    lesson_14_with_pymysql

--- vika_homework/lesson_14/my_database.py
+++ vika_homework/lesson_14/my_database.py
@@ -1,68 +1,84 @@
-import mysql.connector as mysql
+import pymysql
 
-# Твои настройки для XAMPP
-# config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '',  # в XAпше PP по умолчанию пусто
-#     'database': 'my_autotest_db'
-# }
+config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'my_autotest_db',
+    'cursorclass': pymysql.cursors.DictCursor,
+    'autocommit': True
+}
 
 # try:
-#     # Пытаемся подключиться
-#     with mysql.connect(**config) as conn:
-#         if conn.is_connected():
-#             print("✅ Успех! PyCharm видит базу 'my_autotest_db'")
-#
-#             # Проверим, что таблицы на месте
-#             cursor = conn.cursor()
+#     # Все, что внутри try, имеет 4 пробела от края
+#     with pymysql.connect(**config) as conn:
+#         with conn.cursor() as cursor:
 #             cursor.execute("SHOW TABLES")
-#             tables = cursor.fetchall()
+#             print(cursor.fetchall())
 #
-#             print(f"Таблицы в базе: {tables}")
-#
-# except mysql.connector.Error as err:
-#     print(f"❌ Ошибка подключения: {err}")
+# # Блок except должен быть СТРОГО под словом try (0 пробелов отступа друг от друга)
+# except pymysql.MySQLError as err:
+#     print("❌ Ошибка подключения")  # Теперь здесь не будет ошибки
 
 
-db = mysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='my_autotest_db'
-)
+# try:
+#     # Шаг 1: Подключаемся
+#     with pymysql.connect(**config) as db:
+#         with db.cursor() as cursor:
+#             # Шаг 2: Выполняем запрос
+#             cursor.execute('SELECT * FROM Students')
+#
+#             # Шаг 3: Выгружаем данные
+#             data = cursor.fetchall()
+#
+#             # Шаг 4: Печатаем результат (теперь это словари!)
+#             for student in data:
+#                 print(student['last_name'])
+#
+# except pymysql.MySQLError as err:
+#     print(f"❌ Ошибка базы данных: {err}")
 
-cursor = db.cursor(dictionary=True)
+# db = pymysql.connect(
+#     host='localhost',
+#     user='root',
+#     password='',
+#     database='my_autotest_db'
+# )
 
-# cursor.execute('select * from Students')
-#
-# # ШАГ 2: Выгружаем данные из курсора в переменную
-# data = cursor.fetchall()
-#
-# # ШАГ 3: Печатаем результат
-# for student in data:
-#     print(student['last_name'])
+#мой дешманский код вместо красивого вверху (с with и try-except):
+
+db = pymysql.connect(**config)
+cursor = db.cursor()
+
+cursor.execute('select * from Students')
+
+# ШАГ 2: Выгружаем данные из курсора в переменную
+data = cursor.fetchall()
+
+# ШАГ 3: Печатаем результат
+for student in data:
+    print(student['last_name'])
 
 
-# # query = "select * from Students WHERE first_name = '{0}' and last_name = '{1}'" уязвимый для sql-инъекций код
-# # 1. Запрос пишем со специальными плейсхолдерами %s
-# query = "SELECT * FROM Students WHERE first_name = %s AND last_name = %s"
-#
-# # 2. Получаем данные от пользователя
-# f_name = input('Введите имя: ').strip()   # .strip() уберет лишние пробелы
-# l_name = input('Введите фамилию: ').strip()
-#
-# # 3. Передаем данные вторым аргументом в execute (в виде кортежа)
-# cursor.execute(query, (f_name, l_name))
-#
-# data = cursor.fetchall()
-#
-# if not data:
-#     print("Пользователь не найден")
-# else:
-#     print(data)
+# query = "select * from Students WHERE first_name = '{0}' and last_name = '{1}'" уязвимый для sql-инъекций код
+# 1. Запрос пишем со специальными плейсхолдерами %s
+query = "SELECT * FROM Students WHERE first_name = %s AND last_name = %s"
 
-# тройные кавычки позволяют разбивать запрос по строкам для красоты и читаемости
+# 2. Получаем данные от пользователя
+f_name = input('Введите имя: ').strip()   # .strip() уберет лишние пробелы
+l_name = input('Введите фамилию: ').strip()
+
+# 3. Передаем данные вторым аргументом в execute (в виде кортежа)
+cursor.execute(query, (f_name, l_name))
+
+data = cursor.fetchall()
+
+if not data:
+    print("Пользователь не найден")
+else:
+    print(data)
+
+#тройные кавычки позволяют разбивать запрос по строкам для красоты и читаемости
 query = '''
             SELECT Books.title AS book_name, Students.first_name, Students.last_name
             FROM Books
@@ -85,13 +101,10 @@ Insert into Students (first_name, last_name, group_id)
 VALUES (%s, %s, %s)
 '''
 
-data2 = [('Ivan', 'Ivanov', 1), ('Petr', 'Petrov', 1)]
+data2 = [('Sidor', 'Sidorov', 1), ('Jan', 'Moucha', 1)]
 cursor.executemany(insert_query, data2)
 
 cursor.execute("select * from students order by id desc limit 1")
 print(cursor.fetchone())
 
 db.commit()
-
-
-
